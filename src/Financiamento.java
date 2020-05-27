6127159a1b177f218eeeab0bfe3d82cd19361248
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-27 18:26:35 -0300

    Leitura do nome e salario

--- src/Financiamento.java
+++ src/Financiamento.java
@@ -9,7 +9,27 @@ public class Financiamento {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("0.00");
-           
+        
+		String nomeCliente;
+		double salario;
+		
+		//Leitura do nome e salario
+		
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.print("Nome do cliente: ");
+		nomeCliente = sc.nextLine();
+		System.out.print("Salario: ");
+		salario = sc.nextDouble();
+		
+		 while(salario <= 0.00) {
+			 
+			 System.out.println("Salario não pode ser negativo, digite o salario novamente!");
+			 System.out.print("Salario: ");
+			 salario = sc.nextDouble();
+			 
+		 }
+		
+		
 	}
 
 }
