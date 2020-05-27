34665786de8ee972e5ed435fbe3e57b46f42fa27
raphael101216 <raphaelrock_8@hotmail.com>
2020-05-27 20:00:01 -0300

    Processamento do menu

--- src/Financiamento.java
+++ src/Financiamento.java
@@ -12,7 +12,7 @@ public class Financiamento {
 
 		String nomeCliente;
 		double salario, porcentagemEntrada, valorTotalFinan, entrada, valorTotalComEntrada, prestacao, verificaSalario;
-		int numeroPrestacao;
+		int numeroPrestacao, opcao;
 		// Leitura do nome e salario
 
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
@@ -45,7 +45,7 @@ public class Financiamento {
 
 		while (prestacao > verificaSalario) {
 
-			System.out.println("Prestação não pode ser maior que 30% do valor do salário do cliente, tente novamente!");
+			System.out.println("Capacidade de pagamento excedida, tente novamente!");
 			System.out.println("Informe os dados novamente, para continuar...");
 			System.out.print("Salario: ");
 			salario = sc.nextDouble();
@@ -70,8 +70,42 @@ public class Financiamento {
 			prestacao = valorTotalComEntrada / numeroPrestacao;
 			verificaSalario = (salario * 30) / 100;
 		}
+
+		// "Processamento do menu"
+		do {
+			System.out.println();
+			System.out.println("Menu:");
+			System.out.println("1 - Mostrar valor da entrada");
+			System.out.println("2 - Mostrar o valor financiado");
+			System.out.println("3 - Mostrar valor de cada prestação");
+			System.out.println("4 - Sair");
+			System.out.print("Digite uma opcao: ");
+			opcao = sc.nextInt();
+			System.out.println();
+			
+			if (opcao == 1) {
+
+				System.out.print("ENTRADA = R$ " + df.format(entrada));
+				System.out.println();
+				
+			} else if (opcao == 2) {
+
+				System.out.print("VALOR FINANCIADO = R$ " + df.format(valorTotalComEntrada));
+				System.out.println();
+				
+			} else if (opcao == 3) {
+
+				System.out.print("VALOR DE CADA PRESTACAO = R$ " + df.format(prestacao));
+				System.out.println();
+				
+			} else if(opcao <= 0 || opcao > 4) {
+				
+				System.out.println("Opção Invalida, tente novamente!");
+			}
+
+		} while (opcao != 4);
 		
-		
+		 System.out.println("FIM DO PROGRAMA!");
 		
 
 	}
